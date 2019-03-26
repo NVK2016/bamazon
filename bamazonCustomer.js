727bdce3bb8ea1bd59e274add5e3e0206d531324
Namita Shenai <namita.shenai@gmail.com>
2019-03-25 21:33:57 -0700

    display data from MYSQL using npm sql package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,9 @@
 
 //DECLARING ALL REQUIRED VARIABLES 
 //------------------------------------------------------------------
-var sql = require("mysql"); 
-var inquirer = require("inquirer"); 
-var Table = require("cli-table"); 
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
 var clc = require("cli-color");
 
 //=================================Connect to SQL database===============================
@@ -18,8 +18,43 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    
+    // console.log("connected as id " + connection.threadId);
+
+    //If connection is establised sucessfully display inventory 
+    displayInventory();
+
 });
+
+function displayInventory() {
+    console.log(clc.bold("\n Displaying inventory for all supplies..... until stock lasts \n"));
+    // console.log("--------------------------------------------------------------- \n");
+
+    var query = connection.query(
+        "SELECT * FROM products ;",
+        function (err, res) {
+            // Log all results of the SELECT statement
+            //   console.log(res);
+
+            var vertical_table = new Table({
+
+                chars: {
+                    'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+                    , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+                    , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+                    , 'right': '║', 'right-mid': '╢', 'middle': '│'
+                },
+                style: { 'padding-left': 2, 'padding-right': 2 }
+            });
+
+            //Add TABLE HEADERS 
+            vertical_table.push([clc.blue.bold("Item ID"), clc.blue.bold("Product Name"), clc.blue.bold("Department Name"), clc.blue.bold("Price"), clc.blue.bold("Stock Quantity")]);
+
+            //ADD ALL ROWS IN THE TABLE TO DISPLAY INVENTORY 
+            for (var i = 0; i < res.length; i++) {
+                vertical_table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+            }
+            console.log(vertical_table.toString());
+        });
+}
--- schema.sql
+++ schema.sql
@@ -6,10 +6,25 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db; 
 
-create table product(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+create table products(
+    item_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     product_name VARCHAR(150) NOT NULL, 
     department_name VARCHAR(150) NOT NULL, 
     price decimal(10,4) NOT NULL, 
-    stock_quantity int NOT NULL 
+    stock_quantity int(100) NOT NULL 
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sunglasses", "Travel Gear", 153.12, 50), 
+("Clear Globe String Bulbs", "Electronics", 15.95,12), 
+("iphone X", "Electronics",999.09,7), 
+("ipad Air 3", "Electronics",550.05, 32), 
+("Cornhole Game", "Outdoor Games", 99.99, 25 ), 
+("Jenga Blocks", "Outdoor Games", 7.14, 13), 
+("Inflatable Pool Floats - Watermelon", "Travel Gear", 9.99, 35), 
+("SPF 30 Natural Sunscreen", "Skin Care", 12.33, 10), 
+("Aloe Vera Gel", "Skin Care", 12.95, 65); 
+("Scientific Calculator", "Office Supplies", 17.98, 100 ) , 
+("Brother P-touch Label Maker", "Office Supplies", 34.00, 43) ; 
+
+SELECT * FROM products; 
